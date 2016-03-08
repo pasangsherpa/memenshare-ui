6c4f94d08040e025fdf54e02837fa59622839281
Pasang Sherpa <pasang_sh@hotmail.com>
2016-03-08 00:04:11 -0800

    dev server

---
+++ server.js
@@ -0,0 +1,15 @@
+/* eslint-disable no-var, strict */
+const webpack = require('webpack');
+const WebpackDevServer = require('webpack-dev-server');
+const config = require('./webpack/dev.config');
+
+new WebpackDevServer(webpack(config), {
+  publicPath: config.output.publicPath,
+  hot: true,
+  historyApiFallback: true
+}).listen(8000, 'localhost', err => {
+  if (err) {
+    console.log(err);
+  }
+  console.log('Listening at localhost:8000');
+});
