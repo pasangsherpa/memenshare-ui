c857da18fbd48be9fc3cf6d961c2243ef39837d1
Pasang Sherpa <pasang_sh@hotmail.com>
2016-03-10 22:11:50 -0800

    added app route

--- app/routes.js
+++ app/routes.js
@@ -2,10 +2,11 @@ import React from 'react';
 import { Route, IndexRoute } from 'react-router';
 
 /* containers */
+import { App } from './containers/App';
 import { Home } from './containers/Home';
 
 export default (
-  <Route path="/" component={Home}>
+  <Route path="/" component={App}>
     <IndexRoute component={Home} />
     <Route status={404} path="*" component={Home} />
   </Route>
