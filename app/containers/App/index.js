efa674ffbf41e0e3d6a9275762d6aab6b63f166e
Pasang Sherpa <pasang_sh@hotmail.com>
2016-03-11 22:41:52 -0800

    use header component in app

--- app/containers/App/index.js
+++ app/containers/App/index.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router';
+
+/* application components */
+import { Header } from '../../components/Header';
 
 export class App extends Component {
-
+  static propTypes = {
+    children: React.PropTypes.any,
+  };
+  
   render() {
     return (
       <section>
-        <header></header>
+        <Header />
+        {this.props.children}
         <footer></footer>
       </section>
     );
