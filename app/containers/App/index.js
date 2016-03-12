02f329c68fde796e7ec5d8a8cb947cec760970eb
Pasang Sherpa <pasang_sh@hotmail.com>
2016-03-11 22:48:37 -0800

    fix relative to absolute path

--- app/containers/App/index.js
+++ app/containers/App/index.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { Link } from 'react-router';
 
 /* application components */
-import { Header } from '../../components/Header';
+import { Header } from 'components/Header';
 
 export class App extends Component {
   static propTypes = {
